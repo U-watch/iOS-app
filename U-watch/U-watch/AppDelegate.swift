2939367873f8f270ca18cd5e4ca6c454ead797ee
sonnyitsunny <sun9999042@gmail.com>
2024-10-17 22:05:27 +0900

    Implement Google login on client-side

--- U-watch/U-watch/AppDelegate.swift
+++ U-watch/U-watch/AppDelegate.swift
@@ -7,15 +7,34 @@
 
 import UIKit
 
+import GoogleSignIn
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+      var handled: Bool
+
+      handled = GIDSignIn.sharedInstance.handle(url)
+      if handled {
+        return true
+      }
+
+      // Handle other custom URL types.
+
+      // If not handled by this app, return false.
+      return false
+    }
+
+
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Google Sign-In 초기화
+        GIDSignIn.sharedInstance.configuration = GIDConfiguration(clientID: "132938478550-er7oqss7r68r46qsfn01v04uuej9nsib.apps.googleusercontent.com")
         return true
     }
+
 
     // MARK: UISceneSession Lifecycle
 
--- U-watch/U-watch/ViewController.swift
+++ U-watch/U-watch/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 class ViewController: UIViewController {
 
@@ -14,6 +15,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func signIn(sender: Any) {
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { signInResult, error in
+            guard error == nil else {
+                print("Error during sign in: \(String(describing: error?.localizedDescription))")
+                return
+            }
 
+            // 로그인 성공 시 앱의 메인 콘텐츠를 보여주는 코드 추가
+            print("Sign in successful")
+        }
+    }
 }
-

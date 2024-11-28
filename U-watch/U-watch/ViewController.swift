3e0e931adda5626f723159b43d41238cac7d5933
sonnyitsunny <sun9999042@gmail.com>
2024-11-28 20:46:27 +0900

    Implement YouTube API

--- U-watch/U-watch/ViewController.swift
+++ U-watch/U-watch/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  U-watch
-//
-//  Created by 손동현 on 10/9/24.
-//
-
 import UIKit
 import GoogleSignIn
 
@@ -12,17 +5,41 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func signIn(sender: Any) {
-        GIDSignIn.sharedInstance.signIn(withPresenting: self) { signInResult, error in
+        // 요청할 OAuth 범위 설정
+        let additionalScopes = [
+            "https://www.googleapis.com/auth/youtube",
+            "https://www.googleapis.com/auth/youtube.force-ssl"
+        ]
+
+        // Sign-In 요청
+        GIDSignIn.sharedInstance.signIn(withPresenting: self, hint: nil, additionalScopes: additionalScopes) { signInResult, error in
             guard error == nil else {
                 print("Error during sign in: \(String(describing: error?.localizedDescription))")
                 return
             }
 
-            // 로그인 성공 시 앱의 메인 콘텐츠를 보여주는 코드 추가
+            // 로그인 성공 시 사용자 정보를 가져오기
+            guard let user = signInResult?.user else { return }
+
+            // Access Token 출력
+            let accessToken = user.accessToken.tokenString // Optional chaining 제거
+            print("Access Token: \(accessToken)")
+
+            // Refresh Token 출력
+            let refreshToken = user.refreshToken.tokenString // Optional chaining 제거
+            print("Refresh Token: \(refreshToken)")
+
+            // Authorization Code 출력
+            let authorizationCode = user.idToken?.tokenString // idToken은 Optional이므로 ? 유지
+            if let authorizationCode = authorizationCode {
+                print("Authorization Code: \(authorizationCode)")
+            } else {
+                print("No authorization code received.")
+            }
+
             print("Sign in successful")
         }
     }

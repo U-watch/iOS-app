de563e23f5ffeb441d7da9d071afcdb0b6e99d0c
sonnyitsunny <sun9999042@gmail.com>
2024-12-02 22:10:31 +0900

    add WordCloud

--- U-watch/U-watch/Controllers/HomeViewController.swift
+++ U-watch/U-watch/Controllers/HomeViewController.swift
@@ -13,10 +13,12 @@ class HomeViewController: UIViewController {
 
     @IBOutlet weak var testimage: UIImageView!
     
+    @IBOutlet weak var testimage2: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         testimage.image = UIImage(named: "testimage")
-
+        testimage2.image=UIImage(named: "testImage2")
 
     }
 
---
+++ U-watch/U-watch/Models/WordCloud.swift
@@ -0,0 +1,13 @@
+//
+//  WordCloud.swift
+//  U-watch
+//
+//  Created by 손동현 on 12/2/24.
+//
+
+import Foundation
+
+
+struct WordCloudResponse: Codable {
+    let wordCloudImageUrl: URL  // 서버에서 제공하는 워드 클라우드 이미지 URL
+}

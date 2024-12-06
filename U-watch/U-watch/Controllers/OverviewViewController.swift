d9306c57b25c6f9695a8a9f1db9ffa308163f75e
wingyou <winwing.you@gmail.com>
2024-12-06 13:45:05 +0900

    feat: make top keyword cell as custom view

--- U-watch/U-watch/Controllers/OverviewViewController.swift
+++ U-watch/U-watch/Controllers/OverviewViewController.swift
@@ -9,4 +9,21 @@ import UIKit
 
 class OverviewViewController: UIViewController {
     var video: Video?
+    
+    @IBOutlet weak var firstKeywordCell: TopKeywordCell!
+    @IBOutlet weak var secondKeywordSell: TopKeywordCell!
+    @IBOutlet weak var thirdKeywordCell: TopKeywordCell!
+    
+    override func viewDidLoad() {
+        firstKeywordCell.topKeyword = TopKeyword(keyword: "키워드 1", grade: Grade.first, count: 3605)
+        firstKeywordCell.tapCallback = onTopKeywordTap(topKeyword:)
+        secondKeywordSell.topKeyword = TopKeyword(keyword: "키워드 2", grade: Grade.second, count: 1055)
+        secondKeywordSell.tapCallback = onTopKeywordTap(topKeyword:)
+        thirdKeywordCell.topKeyword = TopKeyword(keyword: "키워드 3", grade: Grade.third, count: 203)
+        thirdKeywordCell.tapCallback = onTopKeywordTap(topKeyword:)
+    }
+    
+    func onTopKeywordTap(topKeyword: TopKeyword) {
+        print(topKeyword)
+    }
 }
